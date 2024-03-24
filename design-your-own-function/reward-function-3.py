369bed19308c8c4e022b7de3cc9976365543691f
Kai <77563639+Kai-log-93@users.noreply.github.com>
2024-03-24 23:06:48 +0800

    Update reward-function-3.py

--- design-your-own-function/reward-function-3.py
+++ design-your-own-function/reward-function-3.py
@@ -3,7 +3,7 @@ def reward_function(params):
     # 如果赛车在赛道上并且已经行驶了一些步数，计算奖励
     if params["all_wheels_on_track"] and params["steps"] > 0: 
         # 奖励现在更加重视速度
-        # 速度被提高到四次方以更加强调
+        # 速度被提高到三次方以更加强调
         reward = ((params["progress"] / params["steps"]) * 10) + (params["speed"]**3)
     else:
         # 如果赛车偏离赛道或者还没有开始，奖励是最小的
